99a8ee1c2129ae45d87775e15aa45500cbe01307
Abass Koyang <abasskoyang12345@gmail.com>
2024-01-10 23:11:33 +0930

    revision on react-router basics

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home">
+        <h1>Welcome to the home page!</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint incidunt vel quasi sunt ratione cumque, exercitationem accusantium expedita odio optio esse eos facere reiciendis totam! Ad minus voluptas sunt, odio, adipisci fuga dolore rerum commodi omnis provident natus esse nesciunt illo eos nisi perferendis quo ea ut! Sint, dolor incidunt.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint incidunt vel quasi sunt ratione cumque, exercitationem accusantium expedita odio optio esse eos facere reiciendis totam! Ad minus voluptas sunt, odio, adipisci fuga dolore rerum commodi omnis provident natus esse nesciunt illo eos nisi perferendis quo ea ut! Sint, dolor incidunt.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint incidunt vel quasi sunt ratione cumque, exercitationem accusantium expedita odio optio esse eos facere reiciendis totam! Ad minus voluptas sunt, odio, adipisci fuga dolore rerum commodi omnis provident natus esse nesciunt illo eos nisi perferendis quo ea ut! Sint, dolor incidunt.</p>
+    </div>
+    )
+}
+
+export default Home
